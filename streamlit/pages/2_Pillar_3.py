201646f29f19ede2549bdae935fa699c417ca918
Kichelle <74385536+kichelle-cc@users.noreply.github.com>
2023-03-31 14:39:13 +0100

    Update 2_Pillar_3.py

--- streamlit/pages/2_Pillar_3.py
+++ streamlit/pages/2_Pillar_3.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Pillar 3')
-st.text('''testetsete.''')
+st.text('''Use this page to ...''')
+
+df = pd.read_excel('PILLAR3.xlsx')
+df.rename(columns={'other products':'Other Products'}, inplace=True)
+categories = df.columns[5:11]
+
+options = st.multiselect(
+    'What category of product does your business offer?',
+    sorted(categories),
+    [])
+
+
+if options:
+    st.text('Great, the attributes you need to report on are:')
+    df['mask'] = df[options].apply(lambda x: x.any(), axis=1)
+    st.write(df[df['mask'] == True])
