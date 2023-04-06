b20fc946cd7b57db3aaea8457dc6146c16e4bdc7
Kichelle <74385536+kichelle-cc@users.noreply.github.com>
2023-04-06 08:58:27 +0100

    Update 2_Pillar_3.py

--- streamlit/pages/2_Pillar_3.py
+++ streamlit/pages/2_Pillar_3.py
@@ -1,11 +1,37 @@
 import streamlit as st
 import pandas as pd
 
+def add_logo():
+    st.markdown(
+        """
+        <style>
+        
+            [data-testid="stSidebarNav"]::before {
+                content: "Navigation";
+                margin-left: 20px;
+                margin-top: 0px;
+                font-size: 22px;
+                position: relative;
+                top: 50px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+add_logo()
+
+
 st.title('Pillar 3')
-st.text('''Use this page to ...''')
+st.text('''This app aims to decompose completx regulatory documents into digestable 
+controls and requirements which we can then simplify into physical data attributes''')
 
 df = pd.read_excel('PILLAR3.xlsx')
-df.rename(columns={'other products':'Other Products'}, inplace=True)
+df.rename(columns={
+    'other products':'Other Products',
+    ' Values':'Values',
+    ' Attribute':'Attribute',
+}, inplace=True)
 categories = df.columns[5:11]
 
 options = st.multiselect(
@@ -17,4 +43,19 @@ options = st.multiselect(
 if options:
     st.text('Great, the attributes you need to report on are:')
     df['mask'] = df[options].apply(lambda x: x.any(), axis=1)
-    st.write(df[df['mask'] == True])
+    
+    reported_tbl = df[df['mask'] == True]
+    base = ['Attribute', 'Values']
+    st.write(reported_tbl.reset_index()[base])
+
+    st.download_button(
+                "Export Fully Mapped Attributes to Excel",
+                reported_tbl.reset_index()[base].to_csv(),
+                "sc_reg_attributes.csv",
+                "text/csv",
+                key='download-csv'
+                )
+    # st.write(reported_tbl.iloc[:,:10])
+    # st.write(reported_tbl[options+base])
+    # print((reported_tbl.reset_index()['Attribute ']))
+    # print(reported_tbl.columns)
