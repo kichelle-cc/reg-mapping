8c45cc7b36b36f95eb0c9f803f81224f2c2d761f
kichelle-cc <kichelle.22@gmail.com>
2023-04-13 15:05:58 +0100

    p3 caption

--- streamlit/pages/3_Pillar_3.py
+++ streamlit/pages/3_Pillar_3.py
@@ -26,7 +26,16 @@ add_logo()
 
 st.title('Pillar 3')
 st.divider()
-st.caption('''Pilar 3 is a ...''')
+st.caption('''Pillar 3 is a regulatory requirement for banks, insurers, and other financial 
+institutions to disclose relevant information about their risk management, capital structure, 
+and governance practices. The goal of Pillar 3 is to promote transparency and help stakeholders 
+better understand the risks and financial position of financial institutions. 
+The disclosure requirements cover areas such as credit risk, market risk, liquidity risk, 
+and operational risk, as well as information on capital adequacy, leverage, and funding.
+The disclosures must be made on a regular basis and are typically included in public
+reports such as annual reports, prospectuses, and other regulatory filings.
+Pillar 3 is part of the Basel III framework for bank regulation and is overseen by national 
+regulators.''')
 
 prefix=os.getcwd()+'/streamlit/data/'
 df_all = pd.read_csv(prefix+'Reg & Data Relationship.csv')
@@ -46,32 +55,35 @@ geo_choice = st.selectbox('Geography', np.insert(geos, 0, 'Select a Geography'))
 
 if geo_choice != 'Select a Geography':
 
-    industries = df_all['Industry'].unique()
-    industry_choice = st.multiselect('Industry', industries,max_selections=ms)    
-    if industry_choice:
+    sectors = df_all['Sector'].unique()
+    sector_choice = st.multiselect('Sector', sectors,max_selections=ms)    
+    if sector_choice:
+        # ProductCategories_AssetClasses,Sub-Asset_Class,Asset_Class
+
+        product_categories = df_all['ProductCategories_AssetClasses'].unique()
+        product_choice = st.multiselect('Asset Class', product_categories,max_selections=ms)    
+        if product_choice:
+            options = st.multiselect(
+                'What category of product does your business offer?',
+                sorted(categories),
+                [])
 
 
-        options = st.multiselect(
-            'What category of product does your business offer?',
-            sorted(categories),
-            [])
-
-
-        if options:
-            st.text('Great, the attributes you need to report on are:')
-            df['mask'] = df[options].apply(lambda x: x.any(), axis=1)
-            
-            reported_tbl = df[df['mask'] == True]
-            base = ['Attribute', 'Values']
-            st.write(reported_tbl.reset_index()[base])
-            st.download_button(
-                        "Export Fully Mapped Attributes to Excel",
-                        reported_tbl.reset_index()[base].to_csv(),
-                        "sc_reg_attributes.csv",
-                        "text/csv",
-                        key='download-csv'
-                        )
-            # st.write(reported_tbl.iloc[:,:10])
-            # st.write(reported_tbl[options+base])
-            # print((reported_tbl.reset_index()['Attribute ']))
-            # print(reported_tbl.columns)
+            if options:
+                st.text('Great, the attributes you need to report on are:')
+                df['mask'] = df[options].apply(lambda x: x.any(), axis=1)
+                
+                reported_tbl = df[df['mask'] == True]
+                base = ['Attribute', 'Values']
+                st.write(reported_tbl.reset_index()[base])
+                st.download_button(
+                            "Export Fully Mapped Attributes to Excel",
+                            reported_tbl.reset_index()[base].to_csv(),
+                            "sc_reg_attributes.csv",
+                            "text/csv",
+                            key='download-csv'
+                            )
+                # st.write(reported_tbl.iloc[:,:10])
+                # st.write(reported_tbl[options+base])
+                # print((reported_tbl.reset_index()['Attribute ']))
+                # print(reported_tbl.columns)
